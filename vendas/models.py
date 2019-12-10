70664291386b84401db813559c7cea1b50e005ce
Rauan <rauan.sanfelice@gmail.com>
2019-12-10 13:50:17 -0300

    alterando a estrutura do banco

--- vendas/models.py
+++ vendas/models.py
@@ -4,29 +4,11 @@ class produto(models.Model):
     descricao = models.CharField(max_length=100)
     valor_unid = models.DecimalField(max_digits=9, decimal_places=2)
     data_insercao = models.DateField(auto_now=True)
+    status = models.BooleanField(default=True)
 
-    cho_status = [
-        ('at', 'ativo'),
-        ('ex', 'deletado'),
-    ]
-    status = models.CharField(max_length=2, choices=cho_status, default=cho_status[0][0])
-
-    class Meta:
-        db_table = 'produto'
-        verbose_name_plural = 'produtos'
-
-class venda(models.Model):
+class mesa(models.Model):
     apelido = models.CharField(max_length=50)
-    produto = models.ForeignKey(produto, on_delete=models.PROTECT)
     quantidade = models.IntegerField()
     data_lancamento = models.DateTimeField(auto_now_add=True)
-
-    cho_status = [
-        ('on', 'ativa'),
-        ('of', 'fechada'),
-    ]
-    status = models.CharField(max_length=2, choices=cho_status, default=cho_status[0][0])
-
-    class Meta:
-        db_table = 'venda'
-        verbose_name_plural = 'vendas'
+    data_encerramento = models.DateTimeField(auto_now_add=True)
+    produtos = models.ManyToManyField(produto)
