cc8b9b600b4e3ae57fb6f4ead64c9e7020424a6e
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-01-02 13:07:26 -0300

    Ajustando models

--- vendas/models.py
+++ vendas/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-class produto(models.Model):
+class Produto(models.Model):
     descricao = models.CharField(max_length=100)
     valor_unid = models.DecimalField(max_digits=9, decimal_places=2)
     data_insercao = models.DateField(auto_now_add=True)
     status = models.BooleanField(default=True)
 
-class mesa(models.Model):
+class Mesa(models.Model):
     apelido = models.CharField(max_length=50)
     quantidade = models.IntegerField(null=True)
     data_lancamento = models.DateTimeField(auto_now_add=True)
     data_encerramento = models.DateTimeField(null=True)
-    produtos = models.ManyToManyField(produto)
+    produtos = models.ManyToManyField(Produto)
