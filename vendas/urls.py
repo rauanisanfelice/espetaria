f15e09457e4263817237e9340921b73cfd66b83b
rauanisanfelice <rauan.sanfelice@gmail.com>
2020-01-02 13:56:48 -0300

    Corrigindo erro no metodo Delete

--- vendas/urls.py
+++ vendas/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('mesa-list/', MesaList.as_view(), name='mesa_list'),
     path('mesa-new/', MesaNew.as_view(), name='mesa_new'),
     path('mesa-end/', MesaEnd.as_view(), name='mesa_end'),
-    path('mesa-detail/', MesaDetail.as_view(), name='mesa_detail'),
+    path('mesa-detail/<int:id_mesa>/', MesaDetail, name='mesa_detail'),
 
     # PRODUTOS
     path('produto-list/',ProdutoList.as_view(), name='produto_list'),
