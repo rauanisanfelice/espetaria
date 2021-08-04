1f2c9e72ac2f903c410a444925d67b92cc6b2b40
Rauan Ishida Sanfelice <rauan.sanfelice@gmail.com>
2021-08-04 14:22:48 -0300

    :white_check_mark: Incluindo testes ao add produto

--- app/vendas/tests.py
+++ app/vendas/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from .models import Produto, Venda, Mesa
+from .forms import ProdutoForm
 
-# Create your tests here.
+
+
+class ProdutoTestCase(TestCase):
+
+    def setUp(self):
+        Produto.objects.create(descricao="Queijo", valor_unid=9.99)
+        Produto.objects.create(descricao="Picanha", valor_unid=100.00)
+
+    def test_produto_criado(self):
+        """Teste se os produtos foram criados"""
+        produto_01 = Produto.objects.get(descricao="Queijo")
+        produto_02 = Produto.objects.get(descricao="Picanha")
+        self.assertIsNotNone(produto_01)
+        self.assertIsNotNone(produto_02)
+
+class FormProdutoErrosTestCase(TestCase):
+
+    def test_produto_descricao_null(self):
+        """Teste erro descrição null"""
+
+        error_message = "{'descricao': [{'message': 'Campo obrigatório', 'code': 'required'}]}"
+        data = {"descricao": None, "valor_unid": 9.99 }
+        form = ProdutoForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(str(form.errors.get_json_data(escape_html=False)), error_message)
+
+    def test_produto_valor_null(self):
+        """Teste erro valor unitario null"""
+
+        error_message = "{'valor_unid': [{'message': 'Campo obrigatório', 'code': 'required'}]}"
+        data = {"descricao": "Peixe", "valor_unid": None }
+        form = ProdutoForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(str(form.errors.get_json_data(escape_html=False)), error_message)
+
+    def test_produto_erros_null(self):
+        """Teste erro valor unitario null"""
+
+        error_message = "{'descricao': [{'message': 'Campo obrigatório', 'code': 'required'}], 'valor_unid': [{'message': 'Campo obrigatório', 'code': 'required'}]}"
+        data = {"descricao": None, "valor_unid": None }
+        form = ProdutoForm(data)
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(str(form.errors.get_json_data(escape_html=False)), error_message)
